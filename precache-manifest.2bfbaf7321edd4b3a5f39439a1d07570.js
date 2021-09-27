966fc26cf9255564ab0d702d73b38ca73ce6da29
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2021-09-27 07:03:04 +0200

    Add files via upload

---
+++ precache-manifest.2bfbaf7321edd4b3a5f39439a1d07570.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2117da35d03deedfb6510b3f88538a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49601a2d115bcb5ae7e",
+    "url": "/static/css/2.331ecf4e.chunk.css"
+  },
+  {
+    "revision": "c49601a2d115bcb5ae7e",
+    "url": "/static/js/2.c3503ceb.chunk.js"
+  },
+  {
+    "revision": "644864333dc64001c70c5e25308f0020",
+    "url": "/static/js/2.c3503ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f07b538a70e9eeba39",
+    "url": "/static/js/3.c03e82b8.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.c03e82b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d531832dabe457a36ef",
+    "url": "/static/js/main.da69dc1e.chunk.js"
+  },
+  {
+    "revision": "a0395321500b4ee5d240",
+    "url": "/static/js/runtime-main.6692b40d.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
